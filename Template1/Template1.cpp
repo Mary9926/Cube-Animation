77b5b729ea41cf4e96e30e4ba5f155d462d64492
Maria Gryglewska <maria.gryglewska51@gmail.com>
2020-11-14 13:15:37 +0100

    final cube animation with arrow control

--- Template1/Template1.cpp
+++ Template1/Template1.cpp
@@ -1,5 +1,4 @@
 //cube
-
 #include "stdafx.h"
 #include <Windows.h>
 #include "include\GL\GL.H"
@@ -8,27 +7,11 @@
 
 float zDirection = -8.0;
 float angle = 0.0;
-float XUP[3] = { 1,0,0 }, XUN[3] = { -1, 0, 0 },
-YUP[3] = { 0,1,0 }, YUN[3] = { 0,-1, 0 },
-ZUP[3] = { 0,0,1 }, ZUN[3] = { 0, 0,-1 },
-ORG[3] = { 0,0,0 };
-GLfloat d[3] = { 0.1, 0.1, 0.1 };
-GLfloat viewangle = 0, tippangle = 0, traj[120][3];
-GLfloat  xAngle = 0.0, yAngle = 0.0, zAngle = 0.0;
+float angleTip = 0;
+float angleView = 0;
 
-void MyDisplay() {
-	
-	// The new scene
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glLoadIdentity();
-	glTranslatef(0.0, 0.0, zDirection);
-	glRotatef(tippangle, 1, 0, 0);  // Up and down arrow keys 'tip' view.
-	glRotatef(viewangle, 0, 1, 0);  // Right/left arrow keys 'turn' view.
-	//glRotatef(angle, 1.0, 0.0, 0.0);
-	//glRotatef(angle, 0.0, 1.0, 0.0);
-	//glRotatef(angle, 0.0, 0.0, 1.0);
+void DrawCube() {
 	glBegin(GL_QUADS);
-
 	//front red
 	glColor3f(1.0, 0.0, 0.0);
 	glVertex3f(-1.0, 1.0, 1.0);
@@ -44,7 +27,7 @@ void MyDisplay() {
 	//right blue
 	glColor3f(0.0, 0.0, 1.0);
 	glVertex3f(1.0, 1.0, 1.0);
-	glVertex3f(1.0, -1.0, 1.0);	
+	glVertex3f(1.0, -1.0, 1.0);
 	glVertex3f(1.0, -1.0, -1.0);
 	glVertex3f(1.0, 1.0, -1.0);
 	//left yellow
@@ -66,9 +49,20 @@ void MyDisplay() {
 	glVertex3f(1.0, -1.0, 1.0);
 	glVertex3f(1.0, -1.0, -1.0);
 	glEnd();
+}
+void MyDisplay() {
+	
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	glTranslatef(0.0, 0.0, zDirection);
+	glRotatef(angleTip, 1, 0, 0);  // Up and down arrow keys
+	glRotatef(angleView, 0, 1, 0);  // Right/left arrow keys 
+	//---------random rotation-------------------//
+	/*glRotatef(angle, 1.0, 0.0, 0.0);
+	glRotatef(angle, 0.0, 1.0, 0.0);
+	glRotatef(angle, 0.0, 0.0, 1.0);*/
+	DrawCube();
 	glutSwapBuffers();//swaps the buffers of the current window if double buffered
-
-
 }
 void Reshape(int width, int hight) {
 	glViewport(0, 0,(GLsizei)width,(GLsizei)hight);
@@ -82,47 +76,20 @@ void Reshape(int width, int hight) {
 	glMatrixMode(GL_MODELVIEW); //moves the eye and objects
 }
 
-/*void Keyboard()
+void Arrows(int key, int x, int y)
 {
-	
-}*/
-
-
-
-void Keyboard(unsigned char key, int x, int y)
-{
-	switch (key) {
-
-	case 'j': d[0] += 0.1;  break;
-	case 'k': d[1] += 0.1;  break;
-	case 'l': d[2] += 0.1;  break;
-
-	case 'x': xAngle += 5;  break;
-	case 'y': yAngle += 5;  break;
-	case 'z': zAngle += 5;  break;
-
-	default: printf("Keyboard %c == %d", key, key);
+	switch (key)
+	{
+		case GLUT_KEY_LEFT:  angleView -= 3;  break;
+		case GLUT_KEY_RIGHT:  angleView += 3;  break;
+		case GLUT_KEY_UP:  angleTip -= 3;  break;
+		case GLUT_KEY_DOWN:  angleTip += 3;  break;
 	}
-
 	glutPostRedisplay();
 }
 
-void Special_Keys(int key, int x, int y)
+void Timer(int) 
 {
-	switch (key) {
-
-	case GLUT_KEY_LEFT:  viewangle -= 5;  break;
-	case GLUT_KEY_RIGHT:  viewangle += 5;  break;
-	case GLUT_KEY_UP:  tippangle -= 5;  break;
-	case GLUT_KEY_DOWN:  tippangle += 5;  break;
-
-	default: printf(" Special key %c == %d", key, key);
-	}
-
-	glutPostRedisplay();
-}
-
-void Timer(int) {
 	glutPostRedisplay();
 	glutTimerFunc(1000 / 60, Timer, 0);
 	angle += 1;
@@ -137,20 +104,15 @@ void MyInit() {
 	glEnable(GL_DEPTH_TEST); // enable depht testing
 }
 
-
-
-
 int main(int argc, char** argv) { //<- for normal API
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);//Bit mask to select a window with a depth buffer.
 	glutInitWindowSize(600, 600);//initial window size
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("CUBE");//create widnow, hello title bar
-	
 	glutDisplayFunc(MyDisplay);
 	glutReshapeFunc(Reshape);
-	glutKeyboardFunc(Keyboard);
-	glutSpecialFunc(Special_Keys);
+	glutSpecialFunc(Arrows);
 	glutTimerFunc(0, Timer, 0);
 	MyInit();
 	glutMainLoop();//enter main loop and process events
